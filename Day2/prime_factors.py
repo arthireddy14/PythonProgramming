e64153119d65b6f7995c7f39bff15b21bc521a76
arthireddy14 <chinnintiarthi@gmail.com>
2025-09-09 23:48:10 +0530

    Update prime_factors.py

--- Day2/prime_factors.py
+++ Day2/prime_factors.py
@@ -1,21 +1,21 @@
-# def isprime(n1):
-#     i=2
-#     while(i<=(n/2)):
-#         if((n%i==0)):
-#            return False
+def isprime(n1):
+    i=2
+    while(i<=(n1/2)):
+        if((n1%i==0)):
+           return False
            
-#         i+=1
-#     return True
+        i+=1
+    return True
 
 
+def prime_factors(n):
+    print(f"Prime factors of {n} are:")
+    for i in range(2, n + 1):
+        if n % i == 0 and isprime(i):
+            print(i, end=" ")
 
-# def prime_factors(n):
-#     for i in range(2,n+1):
-#         if(n%i==0):
-#             if(isprime(i)):
-#                 print(i,end=' ')
+num=int(input("Enter a no. "))
+print("Prime factors of ",num," are ")
+prime_factors(num)
 
-# num=int(input("Enter a no. "))
-# print("Prime factors of ",num," are ")
-# prime_factors(num)
 
